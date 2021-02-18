cbd08b684f1a58c886aa5b35863f6f4d86f0efa2
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-18 12:14:58 -0300

    create TheLoading

---
+++ src/components/TheLoading/TheLoading.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { GText } from '@ui';
+
+import loading from '@assets/imgs/loading.png';
+
+import './TheLoading.scss';
+
+const TheLoading = () => {
+  return (
+    <div className="the-loading">
+      <img
+        src={loading}
+        alt="Rick and Morty visiting an alien planet"
+        className="the-loading__img"
+      />
+      <GText classes="the-loading__text">Loading</GText>
+    </div>
+  );
+};
+
+export default TheLoading;
