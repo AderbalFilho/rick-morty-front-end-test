d50273769f8c5a92e8ba394f84ab3141a361f961
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-18 13:45:35 -0300

    create UI GModal

---
+++ src/components/ui/molecules/GModal/GModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+
+function GModal({ children, classes, ...rest }) {
+  const className = classNames('g-modal', classes);
+
+  return (
+    <div className={className} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+GModal.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.string,
+};
+
+export default GModal;
---
+++ src/components/ui/molecules/GModal/index.js
@@ -0,0 +1,3 @@
+import GModal from './GModal';
+
+export default GModal;
