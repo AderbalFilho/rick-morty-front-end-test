ec3ba4e0c3a83a8ccac887a4f9236a99c906f4f4
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-17 23:25:07 -0300

    create TheHome

--- src/pages/TheHome/TheHome.js
+++ src/pages/TheHome/TheHome.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import SearchSection from '@components/SearchSection';
+
+import './TheHome.scss';
+
 function TheHome() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="the-home">
+      <div className="the-home__img-background">
+        <SearchSection />
+      </div>
+    </div>
+  );
 }
 
 export default TheHome;
--- src/pages/TheHome/TheHome.test.js
+++ src/pages/TheHome/TheHome.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 
 import TheHome from './TheHome';
+import { GlobalStorage } from '@contexts/GlobalContext';
 
 describe('TheHome', () => {
   it('matches snapshot', () => {
-    const testRenderer = TestRenderer.create(<TheHome />);
+    const testRenderer = TestRenderer.create(
+      <GlobalStorage>
+        <TheHome />
+      </GlobalStorage>
+    );
     const jsonComponent = testRenderer.toJSON();
 
     expect(jsonComponent).toMatchSnapshot();
