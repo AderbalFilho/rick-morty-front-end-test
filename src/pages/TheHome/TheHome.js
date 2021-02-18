7cd2ecd6c905d79511fec5c586302c37fc5b4de1
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-18 12:15:24 -0300

    add loading to home

--- src/pages/TheHome/TheHome.js
+++ src/pages/TheHome/TheHome.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import classNames from 'classnames/bind';
+
 import SearchSection from '@components/SearchSection';
+import TheLoading from '@components/TheLoading';
+import { GlobalContext } from '@contexts/GlobalContext';
 
 import './TheHome.scss';
 
 function TheHome() {
+  const { isLoading } = useContext(GlobalContext);
+  const className = classNames('the-home', isLoading && ' the-home--blur');
+
   return (
-    <div className="the-home">
-      <div className="the-home__img-background">
-        <SearchSection />
+    <>
+      {isLoading && <TheLoading />}
+      <div className={className}>
+        <div className="the-home__img-background">
+          <SearchSection />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
