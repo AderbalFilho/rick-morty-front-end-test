13cc4ddcaf59ed4e59ec18c1ed6b01732dac206e
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-17 22:53:37 -0300

    add change function to input

--- src/components/ui/atoms/GInput/GInput.test.js
+++ src/components/ui/atoms/GInput/GInput.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import GInput from './GInput';
 
@@ -12,8 +12,21 @@ describe('GInput', () => {
     expect(jsonComponent).toMatchSnapshot();
   });
 
+  it('fire event when input text changed', () => {
+    const onChange = jest.fn();
+    const { getByPlaceholderText } = render(
+      <GInput type="text" placeholder="Search characters" change={onChange} />
+    );
+
+    fireEvent.change(getByPlaceholderText(/Search characters/i), {
+      target: { value: 'inGaia' },
+    });
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
   it('renders classes', () => {
-    const { container } = render(<GInput classes="input-search" />);
+    const { container } = render(<GInput type="text" classes="input-search" />);
     const card = container.querySelector('.g-input');
 
     expect(card.classList).toContain('input-search');
