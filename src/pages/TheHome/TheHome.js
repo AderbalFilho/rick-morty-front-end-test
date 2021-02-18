54c99de639ffbe856f92d5c6f87da867a50b3953
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-18 16:56:14 -0300

    add search to home

--- src/pages/TheHome/TheHome.js
+++ src/pages/TheHome/TheHome.js
@@ -3,12 +3,13 @@ import classNames from 'classnames/bind';
 
 import SearchSection from '@components/SearchSection';
 import TheLoading from '@components/TheLoading';
+import CharacterCard from '@components/CharacterCard';
 import { GlobalContext } from '@contexts/GlobalContext';
 
 import './TheHome.scss';
 
 function TheHome() {
-  const { isLoading } = useContext(GlobalContext);
+  const { characters, isLoading } = useContext(GlobalContext);
   const className = classNames('the-home', isLoading && ' the-home--blur');
 
   return (
@@ -17,6 +18,11 @@ function TheHome() {
       <div className={className}>
         <div className="the-home__img-background">
           <SearchSection />
+          <section className="the-home__cards">
+            {characters.map((character) => (
+              <CharacterCard key={character.id} character={character} />
+            ))}
+          </section>
         </div>
       </div>
     </>
